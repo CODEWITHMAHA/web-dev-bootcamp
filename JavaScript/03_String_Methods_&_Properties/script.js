08af73042243421499b7756a6c052f50cd0ce0f5
Mahrukh <maharukh817@gmail.com>
2025-11-11 13:02:22 +0500

    Variables interview questions

---
+++ JavaScript/03_String_Methods_&_Properties/script.js
@@ -0,0 +1,6 @@
+ let str = "  Hello, World!  ";
+      console.log(str.length); // 17
+      console.log(str.trim()); // "Hello, World!"
+      console.log(str.toUpperCase()); // "  HELLO, WORLD!  "
+      console.log(str.indexOf("World")); // 9
+      console.log(str.slice(2, 7)); // "Hello"
